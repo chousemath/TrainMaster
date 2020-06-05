8a4720d598a1b2ccaa99d60e193e9490072a17da
jochoi <chousemath@gmail.com>
2020-06-06 07:47:25 +0900

    Update

--- App.js
+++ App.js
@@ -21,9 +21,12 @@ import {
 import TouchableImage from './components/TouchableImage';
 import Modal from 'react-native-modal';
 import { colors, colorList } from './libraries/Colors';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
-const mascot = require('./images/colin.png');
+
+const Stack = createStackNavigator();
 
 function LineCard({ line }) {
     return (
@@ -40,7 +43,21 @@ function LineCard({ line }) {
                     paddingTop: 32,
                     paddingLeft: 32,
                 }}>
-                    <Text style={styles.lineCardTitle}>{line.viewValue}</Text>
+                    <Text
+                        numberOfLines={2}
+                        style={styles.lineCardTitle}>
+                        {line.viewValue}
+                    </Text>
+                    <Text
+                        numberOfLines={1}
+                        style={styles.lineCardUpdatedAt}>
+                        최근 퀴즈: 2020.03.15
+                    </Text>
+                    <Text
+                        numberOfLines={1}
+                        style={styles.lineCardUpdatedAt}>
+                        최고 점수: {Math.floor(Math.random() * 55).toString()}
+                    </Text>
                 </View>
             </View>
             <View style={{
@@ -49,14 +66,14 @@ function LineCard({ line }) {
                 height: '100%',
             }}>
                 <Image
-                    source={mascot}
+                    source={line.character}
                     style={styles.mascot}
                 />
             </View>
         </View>
     );
 }
-export default function App() {
+function HomeScreen() {
     return (
         <View style={styles.container}>
             <SafeAreaView style={_flexOne}>
@@ -72,6 +89,29 @@ export default function App() {
     );
 }
 
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    options={{
+                        title: '지하철 노선을 선택하세요!',
+                        headerStyle: {
+                            backgroundColor: '#ffffff',
+                            shadowColor: 'transparent',
+                            shadowRadius: 0,
+                            shadowOffset: { height: 0 },
+                        },
+                        headerTintColor: '#000000',
+                    }}
+                    name='Home'
+                    component={HomeScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
 const styles = StyleSheet.create({
     container: { ..._flexOne, alignItems: 'center', justifyContent: 'center' },
     containerMain: { ..._flexOne },
@@ -81,6 +121,12 @@ const styles = StyleSheet.create({
     },
     lineCardTitle: {
         fontSize: 32,
+        color: '#ffffff',
+        width: windowWidth - 190,
+    },
+    lineCardUpdatedAt: {
+        marginTop: 8,
+        fontSize: 16,
         color: '#ffffff',
     },
     mascot: { width: 150, height: 150, },
